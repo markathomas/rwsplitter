f98d09e85893102e5e88662f9b16d59d1cb2986d
mthomas <spatialguru.net@gmail.com>
2018-09-29 18:45:54 -0500

    Fix FindBugs

--- rwsplitter-jpa/src/main/java/com/elihullc/rwsplitter/jpa/hibernate/hikaricp/implementation/PropertiesFileDataSourceConnectionProvider.java
+++ rwsplitter-jpa/src/main/java/com/elihullc/rwsplitter/jpa/hibernate/hikaricp/implementation/PropertiesFileDataSourceConnectionProvider.java
@@ -3,11 +3,14 @@ package com.elihullc.rwsplitter.jpa.hibernate.hikaricp.implementation;
 import com.elihullc.rwsplitter.jpa.hibernate.hikaricp.HikariCPDataSourceConnectionProvider;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class PropertiesFileDataSourceConnectionProvider extends HikariCPDataSourceConnectionProvider {
 
-    private final Properties properties = new Properties();
+    private static final long serialVersionUID = 4473698965793267295L;
+
+    private Properties properties = new Properties();
 
     public PropertiesFileDataSourceConnectionProvider(final String tenantIdentifier) {
         super(tenantIdentifier);
@@ -16,8 +19,9 @@ public class PropertiesFileDataSourceConnectionProvider extends HikariCPDataSour
     @Override
     protected void beforeConfiguration(final String tenantIdentifier) {
         super.beforeConfiguration(tenantIdentifier);
-        try {
-            this.properties.load(this.getClass().getResourceAsStream("/" + tenantIdentifier + ".properties"));
+        this.properties = new Properties();
+        try (InputStream is = this.getClass().getResourceAsStream("/" + tenantIdentifier + ".properties")) {
+            this.properties.load(is);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
--- rwsplitter-jpa/src/main/java/com/elihullc/rwsplitter/jpa/hibernate/hikaricp/implementation/PropertiesFileMultiTenantConnectionProvider.java
+++ rwsplitter-jpa/src/main/java/com/elihullc/rwsplitter/jpa/hibernate/hikaricp/implementation/PropertiesFileMultiTenantConnectionProvider.java
@@ -6,6 +6,8 @@ import com.elihullc.rwsplitter.jpa.hibernate.SpringTenantIdentifierResolver;
 public class PropertiesFileMultiTenantConnectionProvider
   extends SpringMultiTenantConnectionProvider<PropertiesFileDataSourceConnectionProvider> {
 
+    private static final long serialVersionUID = 7706020243181333623L;
+    
     public PropertiesFileMultiTenantConnectionProvider(final SpringTenantIdentifierResolver tenantIdentifierResolver) {
         super(tenantIdentifierResolver);
     }
